8e2fcd2a12426ee1e00257654ca754e01398f211
Reese Robertson <reese.c.robertson@gmail.com>
2022-12-10 14:47:09 -0800

    Solve day 5 part 1

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -10,10 +10,14 @@ fn main() -> Result<(), Error> {
         Ok(content) => content,
     };
 
-    let yard = parse_yard(&content)?;
+    let mut yard = parse_yard(&content)?;
 
-    for stack in yard.stacks {
-        println!("{:?}", stack);
+    for line in content.split("\n").filter(|line| line.starts_with("move")) {
+        yard.crane_lift(parse_action(line)?)?;
+    }
+
+    for stack in &yard.stacks {
+        println!("{:?}", stack.last());
     }
 
     Ok(())
@@ -33,21 +37,22 @@ impl Yard {
 
     fn crane_lift(&mut self, action: Action) -> Result<()> {
         for _ in 0..action.count {
-            let c = self.stacks[action.source as usize]
+            // Our internal stacks are zero indexed, adjust the index
+            let c = self.stacks[(action.from - 1) as usize]
                 .pop()
-                .ok_or(anyhow!("no crate in stack {}", action.source))?;
+                .ok_or(anyhow!("no crate in stack {}", action.from))?;
 
-            self.stacks[action.destination as usize].push(c);
+            self.stacks[(action.to - 1) as usize].push(c);
         }
 
         Ok(())
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 struct Action {
-    source: u32,
-    destination: u32,
+    from: u32,
+    to: u32,
     count: u32,
 }
 
@@ -56,7 +61,7 @@ fn parse_yard(content: &str) -> Result<Yard> {
     let rows: Vec<_> = content
         .split("\n")
         .map_while(|line| if line.len() == 0 { None } else { Some(line) })
-        .map(|line| parse_line(line))
+        .map(|line| parse_yard_line(line))
         .collect::<Result<_>>()?;
 
     let mut yard = Yard::new();
@@ -84,7 +89,7 @@ fn parse_yard(content: &str) -> Result<Yard> {
 }
 
 /// Parses one row from a Yard's string representation.
-fn parse_line(line: &str) -> Result<Vec<char>> {
+fn parse_yard_line(line: &str) -> Result<Vec<char>> {
     let mut stack_idx = 0;
     let mut row = Vec::new();
     loop {
@@ -107,15 +112,112 @@ fn parse_line(line: &str) -> Result<Vec<char>> {
     Ok(row)
 }
 
+fn parse_action(line: &str) -> Result<Action> {
+    if !line.starts_with("move") {
+        return Err(anyhow!("line does not start with the Action prefix"));
+    }
+
+    let components: Vec<_> = line.split(" ").collect();
+    if components.len() != 6 {
+        return Err(anyhow!("line has wrong number of components for an Action"));
+    }
+
+    return Ok(Action {
+        count: components[1].parse()?,
+        // Defined stack indices are not zero indexed
+        from: components[3].parse::<u32>()?,
+        to: components[5].parse::<u32>()?,
+    });
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_parse_line() -> Result<()> {
-        assert_eq!(parse_line("[A] [B]")?, ['A', 'B']);
-        assert_eq!(parse_line("[A]     [B]")?, ['A', ' ', 'B']);
-        assert_eq!(parse_line("    [A] [B]")?, [' ', 'A', 'B']);
+        assert_eq!(parse_yard_line("[A] [B]")?, ['A', 'B']);
+        assert_eq!(parse_yard_line("[A]     [B]")?, ['A', ' ', 'B']);
+        assert_eq!(parse_yard_line("    [A] [B]")?, [' ', 'A', 'B']);
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_action() -> Result<()> {
+        assert_eq!(
+            parse_action("move 1 from 2 to 3")?,
+            Action {
+                count: 1,
+                from: 2,
+                to: 3
+            },
+        );
+        assert_eq!(
+            parse_action("move 1000 from 20 to 30")?,
+            Action {
+                count: 1000,
+                from: 20,
+                to: 30
+            },
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_action_fails() {
+        assert!(parse_action("notmove 5 from 2 to 3").is_err());
+        assert!(parse_action("move 3 to 3").is_err());
+        assert!(parse_action("move 3 from 3").is_err());
+    }
+
+    #[test]
+    fn test_crane_lift() -> Result<()> {
+        let assert_tops = |yard: &Yard, tops: Vec<char>| {
+            let actual_tops: Vec<_> = yard
+                .stacks
+                .iter()
+                .map(|stack| stack.last())
+                .map(|last_or_none| match last_or_none {
+                    Some(crate_) => *crate_,
+                    None => ' ',
+                })
+                .collect();
+            assert_eq!(actual_tops, tops);
+        };
+
+        {
+            let mut yard = Yard {
+                stacks: vec![vec!['A'], vec!['B']],
+            };
+            yard.crane_lift(parse_action("move 1 from 1 to 2")?)?;
+            assert_tops(&yard, vec![' ', 'A']);
+        }
+
+        {
+            let mut yard = Yard {
+                stacks: vec![vec!['A'], vec!['B', 'C']],
+            };
+            yard.crane_lift(parse_action("move 2 from 2 to 1")?)?;
+            assert_tops(&yard, vec!['B', ' ']);
+        }
+
+        {
+            let mut yard = Yard {
+                stacks: vec![vec!['A'], vec![]],
+            };
+            yard.crane_lift(parse_action("move 1 from 1 to 2")?)?;
+            assert_tops(&yard, vec![' ', 'A']);
+        }
+
+        {
+            let mut yard = Yard {
+                stacks: vec![vec![], vec!['B']],
+            };
+            assert!(yard
+                .crane_lift(parse_action("move 1 from 1 to 2")?)
+                .is_err());
+        }
+
         Ok(())
     }
 }
